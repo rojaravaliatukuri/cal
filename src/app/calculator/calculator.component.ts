a8ad6e02aad4d6b8a88293eaf091cc7ae778ee07
Your Name <you@example.com>
2022-10-30 12:19:48 +0530

    initial commit

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  toshow='0'
+  currvalue=''
+  writetoinput(value:string){
+    this.currvalue=this.currvalue+value
+    this.toshow=this.currvalue
+  }
+  
+  equals(){
+    this.toshow=eval(this.currvalue)
+    this.currvalue=this.toshow
+  }
+
+  clear(){
+    this.currvalue='';
+    this.toshow='0'
+  }
+
+  back(){
+    this.currvalue=this.currvalue.slice(0,-1)
+    this.toshow=this.currvalue
+    if(this.toshow==''){this.toshow='0'}
+  }
+
+  calcvalue(solve:any){
+    if(solve.charAt(0)=='0'){
+      solve = solve.slice(1,)
+    }
+    this.toshow=eval(solve)
+  }
+}
